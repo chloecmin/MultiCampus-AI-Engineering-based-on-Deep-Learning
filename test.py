bf529a95d7cb7d6b2024be14923af7966ea593b8
chloecmin <chloecmin@gmail.com>
2020-07-15 11:21:00 +0900

    _

---
+++ test.py
@@ -0,0 +1,5 @@
+def cal(a,b):
+    return a+b, a-b
+
+result = cal(3, 7)
+print('{} / {}'.format(result[0], result[1]))
--- 가변파라미터.py
+++ 가변파라미터.py
@@ -1,9 +1,19 @@
-#가변 파라미터
-def printNames(*names):
-    for name in names:
-        print(name)
-    print("------------------이번 함수 실행 끝")
-    return
+"""가변 파라미터"""
 
-printNames('이순신', '홍길동', '이순신')
-printNames('강감찬', '권율')
+# def printNames(*names): #튜플형태로 입력
+#     for name in names:
+#         print(name)
+#     print("------------------이번 함수 실행 끝")
+#     return
+#
+# printNames('이순신', '홍길동', '이순신')
+# printNames('강감찬', '권율')
+
+def sum_many(*args):
+    sum = 0
+    for i in args:
+        sum = sum+i
+    return sum
+
+print(sum_many(1,2,3,4,5))
+print(sum_many(10, 20))
